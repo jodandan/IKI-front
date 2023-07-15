2dfeb9183d593f741dd19652f7b88684422691cc
Cherrie <cherriekim925@gmail.com>
2023-07-16 03:28:02 +0900

    [Feat/Cart] 장바구니 화면 내에 상품 들어갈 table 구현

--- src/components/MainPage/cart/cartItems/CartDetail.jsx
+++ src/components/MainPage/cart/cartItems/CartDetail.jsx
@@ -1,18 +1,20 @@
 import { styled } from "styled-components";
+import EachItem from "./EachItem";
 
 const CartDetailBox = styled.div`
   background-color: #9a9a9a;
-  padding: 80px 10px;
+  padding: 30px 10px 15px 10px;
 `;
 
 export default function CartDetail() {
   return (
     <CartDetailBox>
-      <div>장바구니 디테일</div>
       (부드럽게 올라/내려오도록 할 예정)
-      <div>
-        sticky 속성 때문에 밑에 필요없는 공간 생기는 문제 있음 - 추후 수정
+      <div style={{ marginBottom: "30px" }}>
+        sticky 속성 때문에 부모 컴포넌트 하단에 필요없는 공간 생기는 문제 있음 -
+        추후 수정
       </div>
+      <EachItem />
     </CartDetailBox>
   );
 }
---
+++ src/components/MainPage/cart/cartItems/EachItem.jsx
@@ -0,0 +1,39 @@
+import { styled } from "styled-components";
+
+const EachItemTable = styled.table`
+  width: 100%;
+
+  th {
+    padding-bottom: 4px;
+  }
+
+  td {
+    //border: solid black 2px;
+    text-align: center;
+  }
+`;
+
+export default function EachItem() {
+  return (
+    <EachItemTable>
+      <thead>
+        <tr>
+          <th>삭제</th>
+          <th>이름</th>
+          <th>옵션</th>
+          <th>수량</th>
+          <th>가격</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>x</td>
+          <td>메뉴이름</td>
+          <td>차갑게/샷추가</td>
+          <td>2</td>
+          <td>10000원</td>
+        </tr>
+      </tbody>
+    </EachItemTable>
+  );
+}
