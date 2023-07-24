7bb26c48e52fa1119bd0cd0e3a9ba94a0306c953
jodan <wdd789q@naver.com>
2023-07-24 16:36:01 +0900

    [Feat/Login] 회원가입, 로그인 페이지 UI 구현 완료

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import MainPage from "./pages/MainPage";
 import AdminPage from "./pages/AdminPage";
+import SignupPage from './pages/SignupPage';
+
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
         <Route path="/main" element={<MainPage />} />
         <Route path="/admin" element={<AdminPage />} />
       </Routes>
