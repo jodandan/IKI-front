b3803e5fdefea75a6e31edeeb6d9e462051c920f
jodan <wdd789q@naver.com>
2023-07-25 20:54:05 +0900

    [Design/Login] 로그인 회원가입 페이지 UI 수정

--- src/components/LoginPage/Signup.jsx
+++ src/components/LoginPage/Signup.jsx
@@ -18,6 +18,20 @@ const Title = styled.div`
   color:white;
 `;
 
+const Form = styled.form`
+  display: flex;
+  flex-direction: row; /* Change flex-direction to row */
+  align-items: center;
+
+`;
+
+const FormCheck = styled.form`
+  display: flex;
+  flex-direction: row; /* Change flex-direction to row */
+  align-items: center;
+  margin-right: 3rem;
+`;
+
 const SignUpForm = styled.form`
   display: flex;
   flex-direction: column;
@@ -27,6 +41,7 @@ const SignUpForm = styled.form`
 const FormLabel = styled.label`
   font-size: 1.2rem;
   margin-bottom: 0.5rem;
+  margin-right: 1rem;
 `;
 
 const FormInput = styled.input`
@@ -78,55 +93,56 @@ const SignUp = () => {
 
     // 비밀번호 일치 체크
     if (password !== confirmPassword) {
-        alert('비밀번호가 일치하지 않습니다. 다시 입력해주세요.');
-        return;
-      }
+      alert('비밀번호가 일치하지 않습니다. 다시 입력해주세요.');
+      return;
+    }
 
     try {
       // 여기에 회원가입 로직을 수행합니다.
       const response = await axios.post('URL', {
-        nickname:'Jodan',
-        password:'1111',
-        shopName:'스타벅스'
+        nickname: 'Jodan',
+        password: '1111',
+        shopName: '스타벅스'
       }, {
-        
+
       });
       console.log(response)
       console.log('회원가입이 성공하였습니다!', response.data);
       navigate('/signin'); // Redirect to the login page
-      
+
     } catch (error) {
       console.error('회원가입에 실패하였습니다!', error);
     }
   };
 
-//   const handleDuplicateCheck = async () => {
-//     try {
-//       // 여기에 가게명 중복 확인 로직을 수행합니다.
-//       const shopNameResponse = await axios.get('http://43.202.93.57:8080', {
-//         params: {
-//           shopName,
-//         },
-       
-//       });
+  //   const handleDuplicateCheck = async () => {
+  //     try {
+  //       // 여기에 가게명 중복 확인 로직을 수행합니다.
+  //       const shopNameResponse = await axios.get('http://43.202.93.57:8080', {
+  //         params: {
+  //           shopName,
+  //         },
 
-//       if (shopNameResponse.data.isDuplicate) {
-//         setIsshopNameDuplicate(true);
-//       } else {
-//         setIsshopNameDuplicate(false);
-//       }
+  //       });
 
-//       console.log('닉네임 중복 확인이 성공하였습니다!', shopNameResponse.data);
-//     } catch (error) {
-//       console.error('닉네임 중복 확인에 실패하였습니다!', error);
-//     }
-//   };
+  //       if (shopNameResponse.data.isDuplicate) {
+  //         setIsshopNameDuplicate(true);
+  //       } else {
+  //         setIsshopNameDuplicate(false);
+  //       }
+
+  //       console.log('닉네임 중복 확인이 성공하였습니다!', shopNameResponse.data);
+  //     } catch (error) {
+  //       console.error('닉네임 중복 확인에 실패하였습니다!', error);
+  //     }
+  //   };
 
   return (
     <LoginBox>
       <SignUpForm onSubmit={handleSignUp}>
-      <Title>회원가입</Title>
-          <FormLabel htmlFor="nickname">닉네임:</FormLabel>
+        <Title>회원가입</Title>
+        <Form>
+          <FormLabel htmlFor="nickname">사용자명</FormLabel>
           <FormInput
             type="text"
             name="nickname"
@@ -134,7 +150,9 @@ const SignUp = () => {
             onChange={(e) => setNickname(e.target.value)}
             required
           />
-          <FormLabel htmlFor="password">비밀번호:</FormLabel>
+        </Form>
+        <Form>
+          <FormLabel htmlFor="password">비밀번호</FormLabel>
           <FormInput
             type="password"
             name="password"
@@ -142,15 +160,19 @@ const SignUp = () => {
             onChange={(e) => setPassword(e.target.value)}
             required
           />
-          <FormLabel htmlFor="confirmPassword">비밀번호 확인:</FormLabel>
+        </Form>
+        <FormCheck>
+          <FormLabel htmlFor="confirmPassword">비밀번호 확인</FormLabel>
           <FormInput
-          type="password"
-          name="confirmPassword"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          required
+            type="password"
+            name="confirmPassword"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
           />
-          <FormLabel htmlFor="shopName">가게명:</FormLabel>
+        </FormCheck>
+        <Form>
+          <FormLabel htmlFor="shopName">가게이름</FormLabel>
           <FormInput
             type="text"
             name="shopName"
@@ -158,11 +180,12 @@ const SignUp = () => {
             onChange={(e) => setShopName(e.target.value)}
             required
           />
-          <FormInput
+        </Form>
+        <FormInput
           type="submit"
           value="가입하기"
-         />
-      </SignUpForm>  
+        />
+      </SignUpForm>
     </LoginBox>
   );
 };
