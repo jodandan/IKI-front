cc520b0f50dc60f5173605667f1a1859fc916acb
Cherrie <cherriekim925@gmail.com>
2023-07-29 20:15:42 +0900

    [Feat/AdminPage] 커스텀 체크박스 구현

--- src/components/AdminPage/adminOption/TempData.jsx
+++ src/components/AdminPage/adminOption/TempData.jsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { styled } from "styled-components";
+import { FaCheck } from "react-icons/fa6";
 
 const CategoryBox = styled.div`
   background-color: lightgoldenrodyellow;
@@ -8,31 +10,74 @@ const CategoryBox = styled.div`
 const CategoryName = styled.div`
   display: flex;
   align-items: center;
-  padding: 7px 5px;
+  padding: 4px 5px;
   background-color: var(--secondary-color);
 
   span {
+    margin-right: -20px; // checkBox 사이즈만큼
     flex: 1;
     text-align: center;
-  }
-
-  div {
-    display: flex;
-    flex-direction: column;
+    font-weight: bold;
   }
 `;
 
+const CheckArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const CheckboxContainer = styled.div`
+  display: inline-block;
+  width: 20px; // StyledCheckbox랑 동일하게 유지해줘야함
+  height: 20px;
+  cursor: pointer;
+`;
+
+const StyledCheckbox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 20px;
+  height: 20px;
+  background-color: ${({ checked }) =>
+    checked ? "var(--primary-color)" : "white"};
+`;
+
+const HiddenCheckbox = styled.input.attrs({ type: "checkbox" })`
+  display: none;
+`;
+
+const CheckIcon = styled(FaCheck)`
+  color: white;
+`;
+
+const Checkbox = ({ className, checked, onChange, ...props }) => (
+  <CheckboxContainer onClick={onChange} className={className}>
+    <HiddenCheckbox checked={checked} onChange={onChange} {...props} />
+    <StyledCheckbox checked={checked}>
+      <CheckIcon checked={checked} />
+    </StyledCheckbox>
+  </CheckboxContainer>
+);
+
 export default function TempData() {
+  const [isChecked, setIsChecked] = useState(false);
+  const handleCheckbox = () => {
+    setIsChecked((prev) => !prev);
+  };
   const catName = "온도";
 
   return (
     <CategoryBox>
       <CategoryName>
         <span>{catName}</span>
-        <div>
-          <input type="checkbox"></input>
-          <span>필수</span>
-        </div>
+        <CheckArea>
+          <Checkbox checked={isChecked} onChange={handleCheckbox} id="cb1" />
+          <label htmlFor="cb1"></label>
+          <div style={{ fontSize: "9px", marginTop: "2px" }}>필수</div>
+        </CheckArea>
       </CategoryName>
       <div>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
