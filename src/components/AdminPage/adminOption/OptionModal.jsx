979d0287fc6d681ef35bdb9dc88f4db3842b00ab
Cherrie <cherriekim925@gmail.com>
2023-07-29 17:50:12 +0900

    [Feat/AdminPage] 옵션 모달 내부 틀 구축

--- src/components/AdminPage/adminOption/OptionModal.jsx
+++ src/components/AdminPage/adminOption/OptionModal.jsx
@@ -18,32 +18,66 @@ const OptionModalBox = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: space-between;
   background-color: white;
-  height: 55vh;
+  height: 60vh;
   width: 50vw; /* FIXME!! - 추후 수정 */
-  padding: 20px;
   border-radius: 8px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+`;
+
+const OptionContents = styled.div`
+  background-color: lightgoldenrodyellow;
+  width: 100%;
+  overflow-y: auto;
+  div {
+    padding: 20px;
+  }
+`;
+
+const AddButton = styled.div`
+  background: var(--secondary-color);
 `;
 
 const CloseButton = styled.button`
   width: 120px;
   padding: 10px 15px;
+  margin: 15px;
   border: none;
   border-radius: 8px;
   font-weight: bold;
   font-size: 1.1rem;
   background: var(--secondary-color);
   color: var(--primary-color);
+  cursor: pointer;
 `;
 
 const OptionModal = ({ isOpen, onClose }) => {
   // if (!isOpen) return null;
 
+  const menuName = "아메리카노";
+
   return (
     <ModalBackground>
       <OptionModalBox>
-        <div style={{ padding: "30px" }}> 모달입니다~</div>
+        <div
+          style={{
+            margin: "18px",
+            fontWeight: "bold",
+            fontSize: "var(--font-big)",
+          }}
+        >
+          {menuName}
+        </div>
+        <OptionContents>
+          <div>HELLO</div>
+          <div>HELLO</div>
+          <div>HELLO</div>
+          <div>HELLO</div>
+          <div>HELLO</div>
+          <div>HELLO</div>
+          <div>HELLO</div>
+        </OptionContents>
         <CloseButton onClick={onClose}>입력 완료</CloseButton>
       </OptionModalBox>
     </ModalBackground>
