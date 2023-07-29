d57e9c3f73d724ae5fae829f4b7b82500b8690cc
Cherrie <cherriekim925@gmail.com>
2023-07-29 19:32:23 +0900

    [Feat/AdminPage] 옵션 카테고리 이름 컴포넌트 구현

--- src/components/AdminPage/adminOption/TempData.jsx
+++ src/components/AdminPage/adminOption/TempData.jsx
@@ -5,9 +5,35 @@ const CategoryBox = styled.div`
   margin: 6px 17px 25px 17px; // 상 우 하 좌
 `;
 
+const CategoryName = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 7px 5px;
+  background-color: var(--secondary-color);
+
+  span {
+    flex: 1;
+    text-align: center;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
 export default function TempData() {
+  const catName = "온도";
+
   return (
     <CategoryBox>
+      <CategoryName>
+        <span>{catName}</span>
+        <div>
+          <input type="checkbox"></input>
+          <span>필수</span>
+        </div>
+      </CategoryName>
       <div>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
