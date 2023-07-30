26178b2bf04d1405355105d05f64d106785bbd95
Cherrie <cherriekim925@gmail.com>
2023-07-31 01:15:00 +0900

    [Feat/AdminPage] 옵션명/가격 변경 가능하게 수정

--- src/components/AdminPage/adminOption/CategoryData.jsx
+++ src/components/AdminPage/adminOption/CategoryData.jsx
@@ -1,32 +1,92 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import { styled } from "styled-components";
-import CategoryName from "./CategoryName"; // Import the new component
+import CategoryName from "./CategoryName";
+import { FaXmark } from "react-icons/fa6";
 
 const CategoryBox = styled.div`
-  background-color: lightgoldenrodyellow;
+  background-color: lightgreen;
   margin: 6px 17px 25px 17px; // 상 우 하 좌
 `;
 
+const CategoryContents = styled.div`
+  background-color: darkgray;
+  padding: 10px 0;
+  display: grid;
+  gap: 5px;
+`;
+
+const EachOption = styled.div`
+  // each 옵션 이름 & 가격 row
+  display: grid;
+  gap: 15px;
+  grid-template-columns: 4fr 3fr 20px;
+
+  .fixed {
+    background: var(--primary-color);
+    color: white;
+  }
+
+  div {
+    // 옵션이름 칸 그 자체
+    border-radius: 4px;
+    padding: 3px 0;
+    background-color: var(--third-color);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    input {
+      border: none;
+      outline: none;
+      background: none;
+      text-align: center;
+      margin: 0;
+    }
+  }
+`;
+
 export default function TempData() {
+  const [optionName, setOptionName] = useState("");
+  const [optionPrice, SetOptionPrice] = useState("");
   const [isChecked, setIsChecked] = useState(false);
   const handleCheckbox = () => {
     setIsChecked((prev) => !prev);
   };
-  const catName = "온도";
+
+  //const catName = "온도";
 
   return (
     <CategoryBox>
-      {/* Use the new CategoryName component */}
-      <CategoryName
-        catName={catName}
-        isChecked={isChecked}
-        handleCheckbox={handleCheckbox}
-      />
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam,
-      </div>
+      <CategoryName isChecked={isChecked} handleCheckbox={handleCheckbox} />
+      <CategoryContents>
+        <EachOption className="fixed">
+          <div className="fixed">옵션 이름</div>
+          <div className="fixed">가격</div>
+        </EachOption>
+        <EachOption>
+          <div>
+            <input
+              type="text"
+              placeholder="옵션명 입력"
+              value={optionName}
+              onChange={(e) => setOptionName(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              // 옆에 arrow 생기는거 싫어서 일단 type="text"로 해둠
+              type="text"
+              placeholder="가격 입력"
+              step="100"
+              value={optionPrice}
+              onChange={(e) => SetOptionPrice(e.target.value)}
+            />
+          </div>
+          <div style={{ cursor: "pointer" }}>
+            <FaXmark />
+          </div>
+        </EachOption>
+      </CategoryContents>
     </CategoryBox>
   );
 }
