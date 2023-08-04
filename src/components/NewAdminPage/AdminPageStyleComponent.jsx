b0913bbad71f5b3057cd6caaa46f6c9fd24d16b4
songhee Kim <soonghee2@gmail.com>
2023-08-04 17:18:39 +0900

    [feat/newAdminPage] 카테고리 페이지 팝업 기능 구현

--- src/components/NewAdminPage/AdminPageStyleComponent.jsx
+++ src/components/NewAdminPage/AdminPageStyleComponent.jsx
@@ -26,3 +26,13 @@ export const DeleteBtn=styled(Btn)`
     background-color: #ff4949;
     color: white;
 `
+
+export const PopupBox=styled.div`
+    background-color: white;
+    border: 1px solid var(--primary-color);
+    width: 40%;
+    height: 20%;
+    position: fixed;
+    left: 0;
+    right: 0;
+`
