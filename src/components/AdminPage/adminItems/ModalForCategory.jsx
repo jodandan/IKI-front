4a376f8787f5243ab89a7cffe632eb78bd711829
jodan <wdd789q@naver.com>
2023-08-14 01:35:02 +0900

    [feat/AdminCategoryDeleteAPI] 카테고리 수정,삭제 기능 구현 완료

--- src/components/AdminPage/adminItems/ModalForCategory.jsx
+++ src/components/AdminPage/adminItems/ModalForCategory.jsx
@@ -53,6 +53,7 @@ export const EditCategoryModal = ({
   selectedCategoryId,
   selectedCategoryName,
   onClose,
+  onEditCategory,
 }) => {
   const [newCategoryName, setNewCategoryName] = useState(selectedCategoryName);
 
@@ -60,16 +61,26 @@ export const EditCategoryModal = ({
     setNewCategoryName(e.target.value);
   };
 
-  const handleEditCategory = () => {
-    // TODO: 카테고리명을 수정하는 기능을 수행하는 코드 작성
+  const handleEditCategory = async () => {
+    try {
+      // 수정된 카테고리 정보를 백엔드로 전송
+      await axios.put(
+        `${process.env.REACT_APP_SERVER_IP}/api/v1/category/${selectedCategoryId}`,
+        {
+          categoryName: newCategoryName,
+        }
+      );
 
-    // 모달 창 닫기
-    console.log(
-      `${selectedCategoryName}(id=${selectedCategoryId})를 ${newCategoryName}으로 update`
-    );
-    onClose();
+      // 수정 성공 시 모달 창 닫고 업데이트 반영
+      console.log(
+        `${selectedCategoryName}(id=${selectedCategoryId})를 ${newCategoryName}으로 업데이트 되었습니다.`
+      );
+      onClose();
+      onEditCategory();
+    } catch (error) {
+      console.error("카테고리 수정 실패: ", error);
+    }
   };
-
   return (
     <PopupBox>
       <ModalTitle>수정할 카테고리 명을 입력해주세요</ModalTitle>
@@ -84,4 +95,27 @@ export const EditCategoryModal = ({
   );
 };
 
-export default EditCategoryModal;
+export const DeleteCategoryModal = ({ categoryId, categoryName, onClose, onDeleteCategory }) => {
+  const handleDeleteCategory = async () => {
+    try {
+      await axios.delete(
+        `${process.env.REACT_APP_SERVER_IP}/api/v1/category/${categoryId}`
+      );
+      console.log(`${categoryName}(id=${categoryId})가 삭제되었습니다.`);
+      onClose();
+      onDeleteCategory();
+    } catch (error) {
+      console.error("카테고리 삭제 실패: ", error);
+    }
+  };
+
+  return (
+    <PopupBox>
+      <ModalTitle>{categoryName} 카테고리를 삭제하시겠습니까?</ModalTitle>
+      <InputButton onClick={handleDeleteCategory}>삭제</InputButton>
+      <CloseButton onClose={onClose} />
+    </PopupBox>
+  );
+};
+
+
