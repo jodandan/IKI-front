6085945f654dfded93d2f2cb5ef299ec989a9335
jodan <wdd789q@naver.com>
2023-08-14 14:50:57 +0900

    [feat/MenuAPI-jodan] 옵션등록 페이지 수정,삭제 API 구현 완료

--- src/components/AdminPage/adminItems/ModalForOption.jsx
+++ src/components/AdminPage/adminItems/ModalForOption.jsx
@@ -12,6 +12,7 @@ import {
   CheckboxLabel,
 } from "./AdminModalCSS";
 
+//옵션 추가 로직=============================================================
 export const AddOptionModal = ({ onClose, menusId, onAddOption }) => {
   const [isRequired, setIsRequired] = useState(false);
   const [optionGroup, setOptionGroup] = useState("");
@@ -102,52 +103,94 @@ export const AddOptionModal = ({ onClose, menusId, onAddOption }) => {
   );
 };
 
+//옵션 수정 로직================================================================
 export const EditOptionModal = ({
   selectedOptionId,
   selectedOptionData,
   onClose,
+  onEditOption,
 }) => {
-  const [optionData, setOptionData] = useState({
-    name: selectedOptionData.optionName,
-    price: selectedOptionData.optionPrice,
-  });
+  const [inputIsRequired, setInputIsRequired] = useState(selectedOptionData.mandatory);
+  const [inputOptionGroup, setInputOptionGroup] = useState(selectedOptionData.menuOptionsCategory);
+  const [inputOptionName, setInputOptionName] = useState(selectedOptionData.menuOptionsContents);
+  const [inputOptionPrice, setInputOptionPrice] = useState(selectedOptionData.menuOptionsPrice);
 
-  const handleOptionDataChange = (e) => {
-    const { name, value } = e.target;
-    setOptionData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
+  const handleRequiredChange = (e) => {
+    setInputIsRequired(e.target.checked);
   };
 
-  const handleEditOption = () => {
-    // TODO: 옵션을 수정하는 기능을 수행하는 코드 작성
-    console.log(optionData); // 예시: 수정된 옵션 정보를 콘솔에 출력
+  const handleOptionGroupChange = (e) => {
+    setInputOptionGroup(e.target.value);
+  };
 
-    // 모달 창 닫기
-    onClose();
+  const handleOptionNameChange = (e) => {
+    setInputOptionName(e.target.value);
+  };
+
+  const handleOptionPriceChange = (e) => {
+    setInputOptionPrice(e.target.value);
+  };
+
+  const handleEditOption = async () => {
+    try {
+      const updatedOptionData = {
+        menuOptionsCategory: inputOptionGroup,
+        menuOptionsContents: inputOptionName,
+        menuOptionsPrice: inputOptionPrice,
+        mandatory: inputIsRequired,
+      };
+
+      const response = await axios.put(
+        `${process.env.REACT_APP_SERVER_IP}/api/v1/menuoptions/${selectedOptionId}`,
+        updatedOptionData
+      );
+
+      console.log("옵션 수정됨:", response.data);
+      // 모달 창 닫기
+      onClose();
+      onEditOption();
+    } catch (error) {
+      console.error("옵션 수정 실패: ", error);
+    }
   };
 
   return (
     <PopupBox>
       <ModalTitle>옵션 정보를 수정해주세요.</ModalTitle>
+      <CheckboxLabel>
+        <input
+          type="checkbox"
+          checked={inputIsRequired}
+          onChange={handleRequiredChange}
+        />
+        필수
+      </CheckboxLabel>
       <Box>
+        <TitlePlusInput>
+          <InputTitle>종류</InputTitle>
+          <Input
+            type="text"
+            placeholder="옵션 종류"
+            value={inputOptionGroup}
+            onChange={handleOptionGroupChange}
+          />
+        </TitlePlusInput>
         <TitlePlusInput>
           <InputTitle>옵션명</InputTitle>
           <Input
             type="text"
-            name="name"
-            value={optionData.name}
-            onChange={handleOptionDataChange}
+            placeholder="옵션명"
+            value={inputOptionName}
+            onChange={handleOptionNameChange}
           />
         </TitlePlusInput>
         <TitlePlusInput>
           <InputTitle>옵션 가격</InputTitle>
           <Input
             type="number"
-            name="price"
-            value={optionData.price}
-            onChange={handleOptionDataChange}
+            placeholder="옵션 가격"
+            value={inputOptionPrice}
+            onChange={handleOptionPriceChange}
           />
         </TitlePlusInput>
       </Box>
@@ -157,36 +200,27 @@ export const EditOptionModal = ({
   );
 };
 
-export const EditOptionCategoryModal = ({ onClose }) => {
-  const [categoryName, setCategoryName] = useState("");
+// 옵션 삭제 로직=================================================================
+export const DeleteOptionModal = ({ onClose, onDeleteOption, selectedOptionId }) => {
+  const handleDeleteOption = async () => {
+    try {
+      const response = await axios.delete(
+        `${process.env.REACT_APP_SERVER_IP}/api/v1/menuoptions/${selectedOptionId}`
+      );
 
-  const handleCategoryNameChange = (e) => {
-    setCategoryName(e.target.value);
-  };
-
-  const handleEditCategory = () => {
-    // TODO: 카테고리명 수정하는 로직 작성
-    console.log(`수정된 카테고리명: ${categoryName}`);
-    // 모달 창 닫기
-    onClose();
+      console.log("옵션 삭제됨:", response.data);
+      // 모달 창 닫기
+      onClose();
+      onDeleteOption();
+    } catch (error) {
+      console.error("옵션 삭제 실패: ", error);
+    }
   };
 
   return (
     <PopupBox>
-      <ModalTitle>옵션 카테고리명 수정</ModalTitle>
-
-      <Box>
-        <TitlePlusInput>
-          <InputTitle>새로운 카테고리명</InputTitle>
-          <Input
-            type="text"
-            placeholder="카테고리명"
-            value={categoryName}
-            onChange={handleCategoryNameChange}
-          />
-        </TitlePlusInput>
-      </Box>
-      <InputButton onClick={handleEditCategory}>입력 완료</InputButton>
+      <ModalTitle>옵션을 삭제하시겠습니까?</ModalTitle>
+      <InputButton onClick={handleDeleteOption}>삭제</InputButton>
       <CloseButton onClose={onClose} />
     </PopupBox>
   );
